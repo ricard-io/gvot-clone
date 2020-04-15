baec8448afb7752943d5729afa0f9a2dadc69422
François Poulain <francois.poulain@cliss21.org>
2020-04-15 11:40:32 +0200

    feat(html): ajout d un favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,6 +101,7 @@ html_short_title = "Documentation"
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = '../../assets/img/logo.svg'
+html_favicon = '../../assets/img/logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
