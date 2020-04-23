41fccf7ad5dd2e31824190f83da956c95fdf0c9a
François Poulain <francois.poulain@cliss21.org>
2020-04-23 09:58:05 +0200

    fix(pouvoirs): ajoute le collectif à la recherche ; DRY

--- gvot/base/wagtail_hooks.py
+++ gvot/base/wagtail_hooks.py
@@ -80,7 +80,9 @@ class PouvoirAdmin(ModelAdmin):
         'ponderation',
     )
     list_filter = ('scrutin',)
-    search_fields = ('prenom', 'nom', 'courriel', 'contact')
+    search_fields = [
+        s for s in list_display if s not in ['scrutin', 'ponderation']
+    ]
     index_template_name = 'modeladmin/index_pouvoirs.html'
     button_helper_class = PouvoirButtonHelper
 
